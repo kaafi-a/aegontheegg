5a104ce9087ca2e0c446d0f560ef5b779faa8e4f
kaafi <amtul.kafi@covermore.com>
2026-09-16 19:00:00 +0500

    git commit -m "update button click behavior and index.html script tag"

---
+++ app.js
@@ -0,0 +1,6 @@
+const button = document.querySelector("button");
+
+button.addEventListener("click", () => {
+  document.querySelector("h1").textContent = "there is still hope";
+  document.querySelector("p").textContent = "if only all the rich and evil people would get eradicated from earth and sent to jupiter to suffer and all the fasict regimes will come to an end and there is equal distribution of wealth and resources the world might become a better place and yeah FUCK ISRAEL AND F TRUMP AND F NETANYAHU";
+});
